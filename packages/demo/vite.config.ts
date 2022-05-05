24b7c3767c3f16f73eac8daa4668de3426331051
Sorin Gitlan <sgitlan@noction.com>
2022-05-05 23:56:20 +0300

    Moved built demo folder to root

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+
+const outDir = path.resolve(__dirname, '..', '..', 'docs')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir
+  },
   plugins: [vue()]
 })
