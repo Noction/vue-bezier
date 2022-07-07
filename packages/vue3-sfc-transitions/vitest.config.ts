5c0f4fb97c85729e8bedd62270d7ee8b45abd22c
Sorin Gitlan <sgitlan@noction.com>
2022-07-07 10:56:02 +0300

    Trying CodeCov monitor

--- packages/vue3-sfc-transitions/vitest.config.ts
+++ packages/vue3-sfc-transitions/vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   test: {
     coverage: {
       all: true,
-      reporter: ['text', 'lcov'],
+      reporter: ['text', 'json'],
       src: ['src']
     },
     environment: 'jsdom',
