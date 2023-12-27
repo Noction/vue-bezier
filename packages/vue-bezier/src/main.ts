6f9646fb5e5f594fc474c7c6d286e71e91a94691
Sorin Gitlan <sgitlan@noction.com>
2023-12-27 22:42:14 +0200

    fix: changed named export

--- packages/vue-bezier/src/main.ts
+++ packages/vue-bezier/src/main.ts
@@ -1,34 +1,6 @@
-import { App } from 'vue'
-
-import CollapseTransition from './components/Collapse/CollapseTransition.vue'
-
-import FadeTransition from './components/Fade/FadeTransition.vue'
-
-import ScaleTransition from './components/Scale/ScaleTransition.vue'
-
-import SlideXLeftTransition from './components/Slide/SlideXLeftTransition.vue'
-import SlideXRightTransition from './components/Slide/SlideXRightTransition.vue'
-import SlideYDownTransition from './components/Slide/SlideYDownTransition.vue'
-import SlideYUpTransition from './components/Slide/SlideYUpTransition.vue'
-
-import ZoomCenterTransition from './components/Zoom/ZoomCenterTransition.vue'
-import ZoomUpTransition from './components/Zoom/ZoomUpTransition.vue'
-import ZoomXTransition from './components/Zoom/ZoomXTransition.vue'
-import ZoomYTransition from './components/Zoom/ZoomYTransition.vue'
-
-export const components = {
-  CollapseTransition,
-  FadeTransition,
-  ScaleTransition,
-  SlideXLeftTransition,
-  SlideXRightTransition,
-  SlideYDownTransition,
-  SlideYUpTransition,
-  ZoomCenterTransition,
-  ZoomUpTransition,
-  ZoomXTransition,
-  ZoomYTransition
-}
+import * as components from './components'
+export * from './components'
+import type { App } from 'vue'
 
 export function install (app: App) {
   // @ts-expect-error it's a custom flag
@@ -38,8 +10,8 @@ export function install (app: App) {
 
   Object
     .entries(components)
-    .forEach(([key, component]) =>
-      app.component(key, component)
+    .forEach(([componentName, component]) =>
+      app.component(componentName, component)
     )
 }
 
